85ef022c3a34362069553bcd536a9ecc70a44904
songkg7 <56438906+songkg7@users.noreply.github.com>
2021-02-23 02:54:41 +0900

    ref: Q9 변수 nextIdx -> startIdx로 수정

--- Q9.py
+++ Q9.py
@@ -6,13 +6,13 @@ N = int(input())
 limit = [int(input()) for _ in range(N)]
 
 index = 0
-nextIdx = 0
+startIdx = 0
 weights = [] # 병렬로 연결된 로프들이 견딜 수 있는 무게가 저장
 while index != N:
     # print(f'index = {index}')
-    for i in range(nextIdx, len(limit)):
+    for i in range(startIdx, len(limit)):
         rope = [] # 선택한 로프를 담을 배열, index가 증가하고 다시 들어올 때 초기화
-        for j in range(nextIdx, i + 1):
+        for j in range(startIdx, i + 1):
             # print(f'limit[j] = {limit[j]}')
             rope.append(limit[j])
         
@@ -20,33 +20,9 @@ while index != N:
         result = rope[0] * len(rope) # 병렬로 견딜 수 있는 무게
         weights.append(result) # 최대 무게를 구하기 위해 배열에 담아둔다.
 
-    nextIdx += 1
+    startIdx += 1
     index += 1
 weights.sort()
 # print(weights)
 print(weights[-1])
-
-# index = 0
-# nextIdx = 0
-# weights = [] # 병렬로 연결된 로프들이 견딜 수 있는 무게가 저장
-# while index != N:
-#     # print(f'index = {index}')
-#     for i in range(nextIdx, len(limit)):
-#         rope = [] # 선택한 로프를 담을 배열, index가 증가하고 다시 들어올 때 초기화
-#         for j in range(i + 1):
-#             # print(f'limit[j] = {limit[j]}')
-#             rope.append(limit[j])
-        
-#         rope.sort()
-#         print(rope)
-#         result = rope[0] * len(rope) # 병렬로 견딜 수 있는 무게
-#         weights.append(result) # 최대 무게를 구하기 위해 배열에 담아둔다.
-
-#     nextIdx += 1
-
-#     index += 1
-
-# weights.sort()
-# print(weights)
-# print(weights[-1])
 
