16f9eea03d3193f200952e4e165e8f73e5f1775b
songkg7 <56438906+songkg7@users.noreply.github.com>
2021-02-23 00:52:14 +0900

    ref: Q9 필요없는 주석, 변수 제거

--- Q9.js
+++ Q9.js
@@ -1,25 +1,22 @@
 // Clear
 
 let index = 0;
-let limit = [10, 15, 50];
+const limit = [10, 15, 50, 30];
 let weights = [];
 
 while(index != limit.length) {
-    let sum = 0;
     let cnt = 0;
     for (let i = index; i < limit.length; i++) {
       let weight = [];
       console.log(`줄의 갯수 = ${++cnt}`);
       for (let j = index; j <= i; j++) {
-        // console.log(limit[j]);
-        // weight += limit[j];
         weight.push(limit[j]);
       }
       // 들 수 있는 최대 무게 = 무게 안에서 가장 작은 값 * 무게 배열 갯수
-      console.log(`선택한 로프들이 견딜 수 있는 무게 : [${weight}]`);
-      let answer = Math.min.apply(null, weight) * weight.length;
+      console.log(`로프들이 각각 견딜 수 있는 무게 : [${weight}]`);
+      const answer = Math.min.apply(null, weight) * weight.length;
       weights.push(answer);
-      console.log(`줄이 견딜 수 있는 무게 = ${answer}`);
+      console.log(`병렬로 견딜 수 있는 무게 = ${answer}`);
       console.log("============");
     }
     index++;
