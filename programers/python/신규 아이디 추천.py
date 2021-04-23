315ee6bf282cf67164624dac69f6440b8d45f1dd
Haril Song <songkg7@gmail.com>
2021-04-23 17:41:27 +0900

    add : 프로그래머스, 2021 Kakao blind recruitment 신규 아이디 추천

--- programers/python/신규 아이디 추천.py
+++ programers/python/신규 아이디 추천.py
@@ -25,12 +25,36 @@
 #      만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
 # 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
 
-import re
+def solution(new_id):
 
-new_id = "...!@BaT#*..y.abcdefghijklm"
+    # 1단계
+    new_id = new_id.lower()
 
-result1 = new_id.lower()
-result2 = re.sub('[=+,#/\?:^$@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]','', result1)
-result3 = re.sub('.{3}')
+    # 2단계
+    answer = ''
+    for word in new_id:
+        if word.isalnum() or word in '-_.':
+            answer += word
 
-print(result3)
+    # 3단계
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+
+    # 4단계
+    answer = answer[1:] if answer[0] == '.' and len(answer) > 1 else answer
+    answer = answer[:-1] if answer[-1] == '.' else answer
+
+    # 5단계
+    answer = 'a' if answer == '' else answer
+
+    # 6단계
+    if len(answer) >= 16:
+        answer = answer[:15]
+        answer = answer[:-1] if answer[-1] == '.' else answer
+
+    # 7단계
+    if len(answer) < 3:
+        answer = answer + answer[-1] * (2 - len(answer))
+
+    return answer
+
