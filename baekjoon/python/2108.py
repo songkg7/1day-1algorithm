5df935e66e24a2136b67332ab8efd37a1fddf6ac
Haril Song <songkg7@gmail.com>
2021-05-15 18:19:57 +0900

    solve: colletions Counter 활용

--- baekjoon/python/2108.py
+++ baekjoon/python/2108.py
@@ -3,27 +3,43 @@
 
 # 산술평균, 중앙값, 최빈값. 범위 구하기
 
-N = int(input())
+import sys
+from collections import Counter
 
-numbers = sorted([int(input()) for _ in range(N)])
+N = int(sys.stdin.readline())
+
+numbers = sorted([int(sys.stdin.readline()) for _ in range(N)])
+
 
 # 산술평균 : N개의 수들의 합을 N으로 나눈 값
-average = round(sum(numbers) / N)
-print(average)
+def mean(arr):
+    return round(sum(arr) / N)
+
 
 # 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
-median = numbers[round(N / 2)]
-print(median)
+def median(arr):
+    return arr[N // 2]
+
 
 # 최빈값(mode) ; N개의 수들 중 가장 많이 나타나는 값
-modes = sorted(numbers, key=lambda x: numbers.count(x), reverse=True)
-if len(modes) == 1:
-    print(modes[0])
-else:
-    for mode in modes:
-        if mode > modes[0]:
-            print(mode)
-            break
+def mode(arr):
+    mode_dict = Counter(arr)
+    modes = mode_dict.most_common()
+    if len(arr) > 1:
+        if modes[0][1] == modes[1][1]:
+            return modes[1][0]
+        else:
+            return modes[0][0]
+    else:
+        return modes[0][0]
+
 
 # 범위 : N의 수들 중 최댓값과 최솟값의 차이
-print(abs(numbers[0]) + numbers[-1] if len(numbers) > 1 else 0)
+def scope(arr):
+    return max(arr) - min(arr)
+
+
+print(mean(numbers))
+print(median(numbers))
+print(mode(numbers))
+print(scope(numbers))
