498e146f222c340f616ec775361edefaaf5fa050
Haril Song <songkg7@gmail.com>
2021-05-21 02:35:13 +0900

    Add : 백준 백트래킹 N과 M(3) - sol 2

--- baekjoon/python/15651.py
+++ baekjoon/python/15651.py
@@ -1,17 +1,11 @@
 # N과 M (3)
 # 백트래킹
 
-def DFS(L):
-    if L == M:
-        for x in res:
-            print(x, end=' ')
-        print()
-    else:
-        for i in range(1, N + 1):
-            res[L] = i
-            DFS(L + 1)
-
+from itertools import product
 
 N, M = map(int, input().split())
-res = [0] * M
-DFS(0)
+
+nums = [i + 1 for i in range(N)]
+
+for num in product(nums, repeat=M):
+    print(*num)
