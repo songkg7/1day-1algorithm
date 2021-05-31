4d553bbe2442ae63ceb4051248bda664259d1bc4
Haril Song <songkg7@gmail.com>
2021-05-31 23:37:18 +0900

    Add: 백준 <queue> 카드 2

---
+++ baekjoon/python/queue/2164.py
@@ -0,0 +1,13 @@
+# 카드 2
+# queue
+
+from collections import deque
+
+N = int(input())
+
+cards = deque([i for i in range(1, N + 1)])
+while len(cards) > 1:
+    cards.popleft()
+    cards.rotate(-1)
+
+print(cards[0])
