8d02b5b8d8dbd5579311882c1e877ceee83015c8
Haril Song <songkg7@gmail.com>
2021-06-19 19:34:44 +0900

    Solve: dh2

---
+++ codility/dh/task2.py
@@ -0,0 +1,33 @@
+#
+#
+# 2021/06/19 6:58 오후
+
+import itertools
+import numpy
+
+S = "aaaaaaaaa"
+
+
+def solution(S):
+    count = 0
+    if S.count('a') < 3:
+        return 0
+
+    s = list(S)
+
+    for i in range(2, len(s)):
+        for j in range(1, i):
+            arr = numpy.split(s, [j, i])
+            num = set()
+            for k in range(len(arr)):
+                num.add(list(arr[k]).count('a'))
+            print(num)
+            if len(num) == 1:
+                count += 1
+
+    return count
+
+
+print(solution(S))
+
+# ClearTime = 2021/06/19 7:33 오후
