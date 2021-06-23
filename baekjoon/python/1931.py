ef043c181fbf075b642ea2cec8930a033239221f
Haril Song <songkg7@gmail.com>
2021-06-23 13:16:49 +0900

    Retry: 백준 <Greedy> 회의실 배정

--- baekjoon/python/1931.py
+++ baekjoon/python/1931.py
@@ -1,23 +1,21 @@
 # 회의실 배정
-# 탐욕법
+# Greedy
+# 2021/06/23 1:00 오후
 
 # 회의 시간이 짧은 회의들을 최대한 많이 선택하는 것이 정답에 가까워진다.
 
-# TODO: 다시 풀고, 정리하기
+N = int(input())
+times = [list(map(int, input().split())) for _ in range(N)]
 
-n = int(input())
-
-time = [[0] * 2 for _ in range(n)]
-for i in range(n):
-    s, e = map(int, input().split())
-    time[i][0] = s
-    time[i][1] = e
-
-time.sort(key=lambda x: (x[1], x[0]))
+times.sort(key=lambda x: (x[1], x[0]))
 
 count = 1
-end_time = time[0][1]
-for i in range(1, n):
-    if time[i][0] >= end_time:
+end = times[0][1]
+for i in range(1, N):
+    if times[i][0] >= end:
         count += 1
-        end_time = time[i][1]
+        end = times[i][1]
+
+print(count)
+
+# ClearTime = 2021/06/23 1:15 오후
