d307164acd0cf90356c82680212b5030afd17fbd
Haril Song <songkg7@gmail.com>
2021-06-27 23:02:35 +0900

    Solve: PGMS <level 1> 폰켓몬

--- programmers/level1/폰켓몬.py
+++ programmers/level1/폰켓몬.py
@@ -2,16 +2,14 @@
 #
 # 2021/06/27 7:57 오후
 
-from itertools import combinations
-
 nums = [3, 1, 2, 3]
 
 
 def solution(nums):
-    candidates = list(combinations(nums, len(nums) // 2))
-    return max(set(map(lambda x: len(set(x)), candidates)))
+    return min(len(set(nums)), len(nums) // 2)
 
 
 print(solution(nums))
 
 # ClearTime = 2021/06/27 8:08 오후 - 시간초과
+# ClearTime = 2021/06/27 11:01 오후
