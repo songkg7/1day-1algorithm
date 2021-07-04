3196e56efb47b475056d534f1595c2e27e81968f
Haril Song <songkg7@gmail.com>
2021-07-05 06:49:52 +0900

    Refactor: programmers 실패율

--- programmers/kakao/python/실패율.py
+++ programmers/kakao/python/실패율.py
@@ -3,14 +3,15 @@
 # 2021/07/05 4:26 오전
 
 def solution(N, stages):
-    failure = [(i, 1) for i in range(1, N + 1)]
+    failure = {}
+
     for i in range(1, N + 1):
         if stages:
-            failure[i - 1] = (i, stages.count(i) / len(stages))
+            failure[i] = stages.count(i) / len(stages)
             stages = list(filter(lambda x: x >= i + 1, stages))
         else:
-            failure[i - 1] = (i, 0)
+            failure[i] = 0
 
-    return list(map(lambda x: x[0], sorted(failure, key=lambda x: -x[1])))
+    return sorted(failure, key=lambda x: failure[x], reverse=True)
 
 # ClearTime = 2021/07/05 6:11 오전
