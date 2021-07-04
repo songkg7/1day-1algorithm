ce9bb304ca2bf11bb2556f743f548c31e729794e
Haril Song <songkg7@gmail.com>
2021-07-05 06:17:06 +0900

    Solve: programmers <kakao blind recruitment> 실패율

--- programmers/kakao/python/실패율.py
+++ programmers/kakao/python/실패율.py
@@ -2,21 +2,15 @@
 # 2019 Kakao blind recruitment
 # 2021/07/05 4:26 오전
 
-from collections import Counter
-
-N = 6
-stages = [4,4,4,4,4]
-
-
 def solution(N, stages):
-    dp = stages
-    failure = [(1, dp.count(1) / len(dp))] + [(i, 1) for i in range(2, N+1)]
-    for i in range(2, N + 1):
-        if dp:
-            failure[i-1] = (i, dp.count(i) / len(dp))
-            dp = list(filter(lambda x: x >= i + 1, dp))
+    failure = [(i, 1) for i in range(1, N + 1)]
+    for i in range(1, N + 1):
+        if stages:
+            failure[i - 1] = (i, stages.count(i) / len(stages))
+            stages = list(filter(lambda x: x >= i + 1, stages))
+        else:
+            failure[i - 1] = (i, 0)
 
     return list(map(lambda x: x[0], sorted(failure, key=lambda x: -x[1])))
 
-
-print(solution(N, stages))
+# ClearTime = 2021/07/05 6:11 오전
