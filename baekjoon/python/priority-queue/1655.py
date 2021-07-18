3f3b400a475056dbd7aefe37b18895123f65f8b6
Haril Song <songkg7@gmail.com>
2021-07-18 21:34:13 +0900

    solve: BOJ <priority queue> 가운데를 말해요

--- baekjoon/python/priority-queue/1655.py
+++ baekjoon/python/priority-queue/1655.py
@@ -2,18 +2,29 @@
 # priority queue
 # 2021/07/18 9:07 오후
 
-import bisect
-import math
+# TODO: 다시 풀기
 
-nums = []
+import heapq
+import sys
 
-N = int(input())
+left, right = [], []
+N = int(sys.stdin.readline())
 
 for _ in range(N):
-    n = int(input())
-    bisect.insort_left(nums, n)
+    num = int(sys.stdin.readline())
+    if len(left) == len(right):
+        # max heap
+        heapq.heappush(left, (-num, num))
+    else:
+        # min heap
+        heapq.heappush(right, (num, num))
 
-    t = nums[math.ceil(len(nums) / 2 - 1)]
-    print(t)
+    if right and left[0][1] > right[0][1]:
+        left_value = heapq.heappop(left)[1]
+        right_value = heapq.heappop(right)[1]
+        heapq.heappush(right, (left_value, left_value))
+        heapq.heappush(left, (-right_value, right_value))
+
+    print(left[0][1])
 
 # ClearTime = 2021/07/18 9:16 오후 - time over
