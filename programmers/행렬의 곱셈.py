75113142acb7226b364653d4a56eeff3744e6fdc
Haril Song <songkg7@gmail.com>
2021-07-22 14:51:18 +0900

    refactor: programmers <level 2> 행렬의 곱셈 - numpy module 제거

--- programmers/행렬의 곱셈.py
+++ programmers/행렬의 곱셈.py
@@ -1,25 +1,26 @@
 # 연습문제
 # level 2
 
-import numpy as np
-
-#
-# arr1 = [[1, 4],
-#         [3, 2],
-#         [4, 1]]
-# arr2 = [[3, 3],
-#         [3, 3]]
-
-arr1 = [[2, 3, 2],
-        [4, 2, 4],
-        [3, 1, 4]]
-arr2 = [[5, 4, 3],
-        [2, 4, 1],
-        [3, 1, 1]]
+arr1 = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
+arr2 = [[5, 4], [2, 4], [3, 1]]
 
 
 def solution(arr1, arr2):
-    return np.matmul(arr1, arr2)
+    # N * M, M * K 행렬의 곱
+    N = len(arr1)
+    M = len(arr1[0])
+    K = len(arr2[0])
+
+    C = [[0 for _ in range(K)] for _ in range(N)]
+
+    print(C)
+
+    for n in range(N):
+        for k in range(K):
+            for m in range(M):
+                C[n][k] += arr1[n][m] * arr2[m][k]
+
+    return C
 
 
 print(solution(arr1, arr2))
