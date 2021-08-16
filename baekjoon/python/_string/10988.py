c0e64a2542834a9863add1b5ceac4a431f62d716
Haril Song <songkg7@gmail.com>
2021-08-17 00:19:16 +0900

    solve: BOJ <String>

---
+++ baekjoon/python/_string/10988.py
@@ -0,0 +1,9 @@
+# TITLE: 팰린드롬인지 확인하기
+# CATEGORY: String
+# DATE: 2021/08/17 12:12 오전
+
+s = input()
+
+print(1 if s == s[::-1] else 0)
+
+# ClearTime = 2021/08/17 12:14 오전
---
+++ baekjoon/python/_string/1212.py
@@ -0,0 +1,9 @@
+# TITLE: 8진수 2진수
+# CATEGORY: 문자열
+# DATE: 2021/08/17 12:14 오전
+
+s = int(input(), 8)
+
+print(bin(s)[2:])
+
+# ClearTime = 2021/08/17 12:17 오전
