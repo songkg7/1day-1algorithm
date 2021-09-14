d066ba075fccd80507fd880f68972a0a4e88c82f
HarilSong <songkg7@gmail.com>
2021-09-14 22:03:54 +0900

    Solve: BOJ <Math> 상근날드

---
+++ baekjoon/5543.py
@@ -0,0 +1,10 @@
+# TITLE: 상근날드
+# CATEGORY: 수학
+# DATE: 2021/09/14 10:01 오후
+
+burgers = [int(input()) for _ in range(3)]
+drinks = [int(input()) for _ in range(2)]
+
+print(min(burgers) + min(drinks) - 50)
+
+# ClearTime = 2021/09/14 10:03 오후
