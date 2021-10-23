22e00979cac93f8d8528ec4197574081cbbb351b
haril <songkg7@gmail.com>
2021-10-23 21:32:03 +0900

    solve: BOJ <Math> R2

---
+++ baekjoon/3046.py
@@ -0,0 +1,12 @@
+# TITLE: R2
+# CATEGORY: math
+# DATE: 2021/10/23 9:28 오후
+
+# (R1 + R2) / 2 == avg(S)
+# R2 = S * 2 - R1
+
+R1, S = map(int, input().split())
+R2 = S * 2 - R1
+print(R2)
+
+# ClearTime = 2021/10/23 9:30 오후
