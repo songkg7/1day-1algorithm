40251bdf0bc83b9e437da68dc71a4ff3516fbfc4
Haril Song <songkg7@gmail.com>
2023-06-24 18:44:24 +0900

    Time: 1 ms (100%), Space: 44.7 MB (34.59%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -5,7 +5,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
         for (int num : nums) {
-            result = result ^ num;
+            result ^= num;
         }
         return result;
     }
