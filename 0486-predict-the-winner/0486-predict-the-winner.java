5e5227798dbf4346fb3f266da1681e4e8bcc1133
Haril Song <songkg7@gmail.com>
2023-07-28 11:21:30 +0900

    Time: 0 ms (100%), Space: 39.7 MB (86.62%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] dp =  new int[n][n];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i][i] = nums[i];
+        }
+
+        for (int diff = 1; diff < n; diff++) {
+            for (int left = 0; left < n - diff; left++) {
+                int right = left + diff;
+                dp[left][right] = Math.max(nums[left] - dp[left + 1][right], nums[right] - dp[left][right - 1]);
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
+    }
+}
